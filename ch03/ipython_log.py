4862df80fd8db311b0bd9c054437959346e5ad04
Zaighum <zaighum47@gmail.com>
2014-06-21 11:14:56 +0200

    copying

---
+++ ch03/ipython_log.py
@@ -0,0 +1,38 @@
+# IPython log file
+
+img = plt.inread('stinkbug.png')
+img = plt.imread('stinkbug.png')
+get_ipython().magic(u'ls ')
+get_ipython().magic(u'cd ..')
+get_ipython().magic(u'ls ')
+get_ipython().magic(u'cd ch03')
+img = plt.imread('stinkbug.png')
+imshow(img)
+plot(randn(1000).cumsum())
+plot(randn(1000).cumsum())
+get_ipython().magic(u'pinfo %gui')
+2 ** 14314
+2 * 27
+_
+a = _
+q
+a
+foo = 'bar'
+foo
+_i27
+_i19
+_27
+_19
+exec _19
+foo
+get_ipython().magic(u'logstart')
+ip_info = get_ipython().getoutput(u'ipconfig')
+ip_info
+ip_info = get_ipython().getoutput(u'ipconfig eth0 | grep "inet "')
+ip_info
+get_ipython().system(u'ipconfig eth0 | grep "inet "')
+ping = get_ipython().getoutput(u'ping www.google.ca')
+ping
+ping
+get_ipython().magic(u'ls ')
+get_ipython().system(u'git add ipython_log.py')
